989e1065b9c589b866534d8579a0957ee6cb7908
Song Young-jin <myevan80@gmail.com>
2015-03-09 23:43:06 +0900

    ! lua-5.1.5 install fixed @ ports

--- ports/lua-5.1.5/build.sh
+++ ports/lua-5.1.5/build.sh
@@ -1,11 +1,12 @@
-mkdir -p ../../tmp
-pushd ../../tmp
-#wget http://www.lua.org/ftp/lua-5.1.5.tar.gz
-#tar -zxvf lua-5.1.5.tar.gz
+mkdir -p ../../prebuilts/lua-5.1.5/
+mkdir -p ../../sources/
+pushd ../../sources/
+wget http://www.lua.org/ftp/lua-5.1.5.tar.gz
+tar -zxvf lua-5.1.5.tar.gz
 cd lua-5.1.5
 cp -r ../../ports/lua-5.1.5/* .
 mkdir -p build
 cd build
-cmake ..
-make
+cmake .. -DCMAKE_INSTALL_PREFIX=../../../prebuilts/lua-5.1.5/
+make install
 popd
