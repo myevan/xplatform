dfef09b298e42ebfa962eb36fb518ecb573d41ea
Song Young-jin <myevan80@gmail.com>
2015-03-13 23:45:16 +0900

    ! prepare project for nt, posix

--- ports/lua-5.1.5/install.py
+++ ports/lua-5.1.5/install.py
@@ -3,6 +3,7 @@ import urllib
 import tarfile
 import shutil
 
+from urlparse import urlparse
 
 def prepare_directory(path):
     if os.access(path, os.R_OK):
@@ -34,29 +35,41 @@ def copy_files(source_dir_path, target_dir_path):
             shutil.copyfile(source_file_path, target_file_path)
 
 
-def prepare_project(cmake_exe_path, project_dir_path, system_dir_path):
-    os.system('''"{0}" {1} -DCMAKE_INSTALL_PREFIX={2}'''.format(cmake_exe_path, project_dir_path, system_dir_path))
+def find_cmake_abs_path():
+    if os.name == 'nt': 
+        return "c:/Program Files (x86)/CMake/bin/cmake.exe"
+    else:
+        return "cmake"
 
+def prepare_project(working_dir_abs_path, remote_archive_uri):
+    archive_file_name = os.path.split(urlparse(remote_archive_uri).path)[1]
+    if archive_file_name.endswith('.gz'):
+        archive_name = os.path.splitext(os.path.splitext(archive_file_name)[0])[0]
+    else:
+        archive_name = os.path.splitext(archive_file_name)[0]
 
-remote_archive_uri = "http://www.lua.org/ftp/lua-5.1.5.tar.gz"
+    local_archive_abs_path = os.path.join(SOURCES_DIR_ABS_PATH, archive_file_name)
+    source_dir_abs_path = os.path.join(SOURCES_DIR_ABS_PATH, archive_name)
+    prebuilt_dir_abs_path = os.path.join(PREBUILTS_DIR_ABS_PATH, archive_name)
+    build_dir_abs_path = os.path.join(source_dir_abs_path, 'build')
 
-CMAKE_EXE_ABS_PATH = "c:/Program Files (x86)/CMake/bin/cmake.exe"
+    prepare_directory(SOURCES_DIR_ABS_PATH)
+    download_file(remote_archive_uri, local_archive_abs_path)
+    extract_file(local_archive_abs_path, SOURCES_DIR_ABS_PATH)
+    copy_files(working_dir_abs_path, source_dir_abs_path)
+    prepare_directory(prebuilt_dir_abs_path)
+    prepare_directory(build_dir_abs_path)
+
+    os.chdir(build_dir_abs_path)
+    os.system('''"{0}" {1} -DCMAKE_INSTALL_PREFIX={2}'''.format(CMAKE_EXE_ABS_PATH, source_dir_abs_path, prebuilt_dir_abs_path))
+   
+
 SOURCES_DIR_REL_PATH = "../../sources"
 PREBUILTS_DIR_REL_PATH = "../../prebuilts"
-MAIN_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-SOURCES_DIR_ABS_PATH = os.path.normpath(os.path.join(MAIN_DIR_PATH, SOURCES_DIR_REL_PATH))
-PREBUILTS_DIR_ABS_PATH = os.path.normpath(os.path.join(MAIN_DIR_PATH, PREBUILTS_DIR_REL_PATH))
-local_archive_abs_path = os.path.join(SOURCES_DIR_ABS_PATH, 'lua-5.1.5.tar.gz')
-source_dir_abs_path = os.path.join(SOURCES_DIR_ABS_PATH, 'lua-5.1.5')
-prebuilt_dir_abs_path = os.path.join(PREBUILTS_DIR_ABS_PATH, 'lua-5.1.5')
-build_dir_abs_path = os.path.join(source_dir_abs_path, 'build')
+CMAKE_EXE_ABS_PATH = find_cmake_abs_path()
+MODULE_DIR_ABS_PATH = os.path.dirname(os.path.realpath(__file__))
+SOURCES_DIR_ABS_PATH = os.path.normpath(os.path.join(MODULE_DIR_ABS_PATH, SOURCES_DIR_REL_PATH))
+PREBUILTS_DIR_ABS_PATH = os.path.normpath(os.path.join(MODULE_DIR_ABS_PATH, PREBUILTS_DIR_REL_PATH))
 
-prepare_directory(SOURCES_DIR_ABS_PATH)
-download_file(remote_archive_uri, local_archive_abs_path)
-extract_file(local_archive_abs_path, SOURCES_DIR_ABS_PATH)
-copy_files(MAIN_DIR_PATH, source_dir_abs_path)
-prepare_directory(prebuilt_dir_abs_path)
-prepare_directory(build_dir_abs_path)
-os.chdir(build_dir_abs_path)
-prepare_project(CMAKE_EXE_ABS_PATH, source_dir_abs_path, prebuilt_dir_abs_path)
+install_project(os.getcwd(), "http://www.lua.org/ftp/lua-5.1.5.tar.gz")
 
